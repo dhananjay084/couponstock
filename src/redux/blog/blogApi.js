8866fbec0fc21c6a9cfcf6c1b3660a4dad592098
Dhananjay Bansal <dhananjaybansal@Dhananjays-MacBook-Air.local>
2025-08-20 11:30:58 +0530

    pull changes

--- src/redux/blog/blogApi.js
+++ src/redux/blog/blogApi.js
@@ -1,10 +1,6 @@
 import axios from 'axios';
 
-<<<<<<< HEAD
-const API_URL = 'http://localhost:5000/api/blogs'; // Update if needed
-=======
 const API_URL = `${process.env.NEXT_PUBLIC_SERVER_URL}/api/blogs`;// Update if needed
->>>>>>> 81aac881e51f56a2eff8a14129d6851d7821b1e4
 
 export const getAllBlogs = () => axios.get(API_URL);
 export const addBlog = (data) => axios.post(API_URL, data);
